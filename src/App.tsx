25886131f244b0074fa5c6c52fe8940b0a3af655
k.shibata <k.shibata@avielan.co.jp>
2022-07-11 11:19:10 +0900

    initial commit with superplate

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { Refine } from "@pankod/refine-core";
+import routerProvider from "@pankod/refine-react-router-v6";
+import { DataProvider } from "@pankod/refine-strapi-v4";
+import { useTranslation } from "react-i18next";
+
+import { authProvider, axiosInstance } from "./authProvider";
+import { API_URL } from "./constants";
+
+function App() {
+  const { t, i18n } = useTranslation();
+
+  const i18nProvider = {
+    translate: (key: string, params: object) => t(key, params),
+    changeLocale: (lang: string) => i18n.changeLanguage(lang),
+    getLocale: () => i18n.language,
+  };
+
+  return (
+    <Refine
+      routerProvider={routerProvider}
+      authProvider={authProvider}
+      dataProvider={DataProvider(API_URL + `/api`, axiosInstance)}
+      i18nProvider={i18nProvider}
+    />
+  );
+}
+
+export default App;
